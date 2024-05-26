30b08738cdf0e7c41f14e2b84e921eac822add13
LouisC-1952673 <louis.caerts@student.uhasselt.be>
2024-05-26 17:07:24 +0200

    Blank authentication page creation
    Test to see if I can add a new blank page to the site that can be navigated to.

---
+++ app/auth/page.jsx
@@ -0,0 +1,19 @@
+export const metadata = {
+    title: 'Authentication'
+};
+
+const explainer = `
+This page contains the login & registration functionality of the web app.
+Users must be logged in in order to access the core of the app.
+`;
+
+export default function Page() {
+    return (
+        <main className="flex flex-col gap-8 sm:gap-16">
+            <section className="flex flex-col items-start gap-3 sm:gap-4">
+                <h1 className="mb-0">Authentication</h1>
+                <p className="text-lg">Let's get you logged in!</p>
+            </section>
+        </main>
+    );
+}
