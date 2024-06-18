bb624fd8970af1d6434e4a8290b582cacbd63292
LouisC-1952673 <louis.caerts@student.uhasselt.be>
2024-06-18 08:25:40 +0200

    pwa basic setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    sw: 'sw.js',
+    runtimeCaching: [
+      {
+        urlPattern: /.*\.(?:png|gif|jpg)/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'images',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          },
+        },
+      },
+      {
+        urlPattern: /.*\.(?:css|js|scss)/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'assets',
+        },
+      },
+      {
+        urlPattern: new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          plugins: [
+            {
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          ],
+        },
+      },
+    ],
+});
+
+module.exports = withPWA({
     reactStrictMode: true,
     swcMinify: true
-};
-
-module.exports = nextConfig;
+});
